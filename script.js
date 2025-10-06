67afeb82250c2215b09b68d3ccbb9799d4e4e47d
Anthony Burgic <144724632+AnthonyBurgic1@users.noreply.github.com>
2025-10-06 10:22:05 -0400

    I have added the followng code elements in the script.js file:
    const subject, const verbs, const adjecrives, const objects, const places

---
+++ script.js
@@ -0,0 +1,5 @@
+const subjects = ["The cat", "My teacher", "A robot", "An alien", "The pirate"];
+const verbs = ["found", "ate", "painted", "lost", "built"];
+const adjectives = ["a shiny", "a funny", "a gigantic", "a tiny", "a mysterious"];
+const objects = ["apple", "spaceship", "castle", "book", "treasure"];
+const places = ["in the forest", "on the moon", "at school", "under the sea", "in Italy"];
