4a36bde9d567a18b7106b395656799bd184d55f9
Anthony Burgic <144724632+AnthonyBurgic1@users.noreply.github.com>
2025-10-06 10:23:54 -0400

    on lines 7 - 13 i have added a let choices = { tag
    in that tag i have added: 
    
    subject: "",
      verb: "",
      adjective: "",
      object: "",
      place: ""

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const verbs = ["found", "ate", "painted", "lost", "built"];
 const adjectives = ["a shiny", "a funny", "a gigantic", "a tiny", "a mysterious"];
 const objects = ["apple", "spaceship", "castle", "book", "treasure"];
 const places = ["in the forest", "on the moon", "at school", "under the sea", "in Italy"];
+
+let choices = {
+subject: "",
+verb: "",
+adjective: "",
+object: "",
+place: ""
+}
